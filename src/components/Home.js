5d6ddf8cef0f232eb49cb6931be70b40d162ec21
UttkarshKhanke <uttkarsh.khanke@gmail.com>
2025-02-06 02:27:58 +0530

    removed logo from homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,9 @@
 import React from "react";
 import './Home.css'; // Assuming you are using a separate CSS file for styling
-import homeimage from '../assets/homeimage.png';
 
 function Home() {
   return (
     <div className="home-container">
-      <div className="home-image-container">
-        <img src={homeimage} alt="Militrack" className="homeimage" />
-      </div>
       <h1 className="home-title">Welcome to Miilitrack</h1>
     </div>
   );
